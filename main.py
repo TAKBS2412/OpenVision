abcbf01f4a2084e12179e08db08b706dfd5b0773
Timothy Eng <timothyeng36@gmail.com>
2017-01-25 18:29:45 -0800

    Added distance and angle calculations.

--- main.py
+++ main.py
@@ -13,13 +13,21 @@ def main():
 	resolution = (imgwpx, imghpx) 
 	camera = image.initCamera(resolution)
 	img = image.takePicture(camera)
-	img = image.procImage(img, resolution, 90, 50, 50, 130, 255, 255)
-	cv2.imshow("Frame", img)
-	cv2.waitKey()
+	img = image.procImage(img, resolution, 80, 40, 0, 130, 255, 200)
+
 	largestCnt = image.getLargestContour(img)
-	print(largestCnt)
 	boundingrect = cv2.minAreaRect(largestCnt)
 	wpx, hpx = boundingrect[1]
-	viewangle = 0.698/2.0
-	print("Distance (cm): " + str(image_proc.getDistance(imghpx, 20, hpx, viewangle)))
+	viewangle = 0.698
+
+	print("Height (px): " + str(hpx))
+	print("Width (px): " + str(wpx))
+	distance = image_proc.getDistance(imghpx, 5.08, hpx, viewangle)
+	print("Distance (cm): " + str(distance))
+	print("Angle (radians): " + str(image_proc.getHorizAngle(imghpx, 5.08, distance, hpx, image.getContourCentroidCoords(largestCnt)[0])))
+
+	cv2.imshow("Frame", img)
+	cv2.waitKey(0)
+
+
 main()
