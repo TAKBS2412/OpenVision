e3bfa6cb39dc4900593df644427936a5ac5eff1f
Timothy Eng <timothyeng36@gmail.com>
2017-01-26 18:09:07 -0800

    Calculate distance and angle in test_video.py

--- test_video.py
+++ test_video.py
@@ -4,6 +4,7 @@ from picamera import PiCamera
 import time
 import cv2
 import image # Custom library
+import image_proc # Another custom library
 import numpy as np
 
 # Initialize the camera
@@ -17,6 +18,10 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 # Flags for image processing
 filterHSV = False # Whether we apply HSV filtering or not
 
+# Image resolution
+resolution = camera.resolution
+imgwpx, imghpx = resolution
+
 # Capture and display frames from camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):	
 	
@@ -24,22 +29,34 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	key = cv2.waitKey(1)
 
 	# Grab array representing image
-	image = frame.array
+	img = frame.array
 
 	if key == ord("f"):
 		filterHSV = not filterHSV
-
+	
 	if filterHSV:
 		# HSV filter the image
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 		lower_range = np.array([80, 40, 0])
 		higher_range = np.array([130, 255, 200])
-		HSVmask = cv2.inRange(image, lower_range, higher_range)
-		image = cv2.bitwise_and(image, image, mask=HSVmask)
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		HSVmask = cv2.inRange(img, lower_range, higher_range)
+		img = cv2.bitwise_and(img, img, mask=HSVmask)
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		if key == ord("p"):
+			# Process the image
+			largestCnt = image.getLargestContour(img)
+			boundingrect = cv2.minAreaRect(largestCnt)
+			wpx, hpx = boundingrect[1]
+			viewangle = 0.698
+
+			print("Height (px): " + str(hpx))
+			print("Width (px): " + str(wpx))
+			distance = image_proc.getDistance(imghpx, 5.08, hpx, viewangle)
+			print("Distance (cm): " + str(distance))
+			print("Angle (radians): " + str(image_proc.getHorizAngle(imghpx, 5.08, distance, hpx, image.getContourCentroidCoords(largestCnt)[0])))
 
 	# Show the frame
-	cv2.imshow("Frame", image)
+	cv2.imshow("Frame", img)
 	
 	# Clear the stream for the next frame
 	rawCapture.truncate(0)
