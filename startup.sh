7d4293b60112ccd0831c0ca4470957837a684113
Timothy Eng <timothyeng36@gmail.com>
2017-03-28 21:47:39 -0700

    Redirect ping output to a file.

--- startup.sh
+++ startup.sh
@@ -3,8 +3,7 @@ source ~/.profile
 workon cv
 
 # Ping 10 times
-
-ping 10.24.12.1 -c 10
+ping 10.24.12.1 -c 10 > /home/pi/src/logs/ping.log
 
 echo "-----------------------" >> /home/pi/src/logs/output.log
 python nt_client.py >> /home/pi/src/logs/output.log 2>&1
