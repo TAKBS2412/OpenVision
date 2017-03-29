8f758fffe3fa8dd0c8433ee43fee64e7af840763
Timothy Eng <timothyeng36@gmail.com>
2017-03-28 21:46:34 -0700

    Ping 10.24.12.1 when startup.sh runs.

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 source ~/.profile
 workon cv
+
+# Ping 10 times
+
+ping 10.24.12.1 -c 10
+
 echo "-----------------------" >> /home/pi/src/logs/output.log
 python nt_client.py >> /home/pi/src/logs/output.log 2>&1
