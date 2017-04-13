1ecb3f9e0f7c3e06cd1031d8d5f1cf8dad7af40e
Timothy Eng <timothyeng36@gmail.com>
2017-04-12 17:35:47 -0700

    Created a script for finding matches using SIFT's BFMatcher.

---
+++ SIFT_BF.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+
+'''
+An example of a Brute Force matcher using SIFT's BFMatcher.
+See docs.opencv.org/trunk/dc/dc3/tutorial_py_matcher.html
+'''
+
+img1 = cv2.imread('test.jpg',0)	   # queryImage
+img2 = cv2.imread('cropped.jpg',0) # trainImage
+
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+# BFMatcher with default params
+bf = cv2.BFMatcher()
+matches = bf.knnMatch(des1, des2, k=2)
+
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+	if m.distance < 0.7*n.distance:
+		good.append(m)
+
+img3 = None
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good, img3, flags=2)
+cv2.imshow("Matches", img3)
+cv2.waitKey(0)
+
+
