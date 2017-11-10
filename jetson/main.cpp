de0380db8b3238221d2150bcd37a01f954c30a5d
Timothy Eng <timothyeng36@gmail.com>
2017-11-10 02:05:14 +0000

    Filter the image and find the contours.

--- jetson/main.cpp
+++ jetson/main.cpp
@@ -1,13 +1,27 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
 
 int main() {
-	//cv::Mat img(512, 512, CV_8UC3, cv::Scalar(0));
 	cv::Mat img;
 
-	img = cv::imread("/home/ubuntu/src/jetson/test.png", cv::IMREAD_COLOR);
-	//cv::putText(img, "Hello, world!", cv::Point(10, img.rows/2), cv::FONT_HERSHEY_DUPLEX, 1.0, CV_RGB(118, 185, 0), 2);
+	img = cv::imread("/home/ubuntu/src/jetson/faketargets.png", cv::IMREAD_COLOR);
 	cv::cvtColor(img, img, CV_BGR2HSV);
+	int lowerh = 50;
+	int lowers = 200;
+	int lowerv = 30;
+	int higherh = 66;
+	int highers = 255;
+	int higherv = 255;
+
+	cv::inRange(img, cv::Scalar(lowerh, lowers, lowerv), cv::Scalar(higherh, highers, higherv), img);
+	
+	cv::vector<cv::vector<cv::Point> > contours;
+	cv::vector<cv::Vec4i> hierarchy;
+
+	cv::findContours(img.clone(), contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_TC89_KCOS);
+
+	std::cout << contours.size();
 
 	cv::imshow("Hello!", img);
 	cv::waitKey();
