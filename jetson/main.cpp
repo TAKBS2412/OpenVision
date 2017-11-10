133b7b016577dd78cb7650b25d52327bb11e15c0
Timothy Eng <timothyeng36@gmail.com>
2017-11-10 03:52:59 +0000

    Display the contours.

--- jetson/main.cpp
+++ jetson/main.cpp
@@ -4,6 +4,7 @@
 
 int main() {
 	cv::Mat img;
+	cv::Mat newimg(480, 640, CV_8UC3, cv::Scalar(0, 0, 0));
 
 	img = cv::imread("/home/ubuntu/src/jetson/faketargets.png", cv::IMREAD_COLOR);
 	cv::cvtColor(img, img, CV_BGR2HSV);
@@ -20,9 +21,53 @@ int main() {
 	cv::vector<cv::Vec4i> hierarchy;
 
 	cv::findContours(img.clone(), contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_TC89_KCOS);
+	cv::vector<cv::Point> largestContour;
+	cv::vector<cv::Point> secondLargestContour;
 
-	std::cout << contours.size();
+	if(contours.size() < 2) {
+		std::cout << "Not enough contours!\n";
+	} else {
+		double largestContourArea = 0; // Area of the largest contour
+		double secondLargestContourArea = 0; // Area of the largest contour
+				
+		int i;
+		for(i = 0; i < contours.size(); i++) {
+			cv::vector<cv::Point> currentContour = contours[i];
+			double contourArea = cv::contourArea(currentContour);
+			cv::vector<cv::Point> approx;
+			cv::approxPolyDP(currentContour, approx, 0.05*cv::arcLength(currentContour, true), true);
 
-	cv::imshow("Hello!", img);
+			if(approx.size() != 4) {
+				continue;
+			}
+
+			double polygonArea = cv::contourArea(approx);
+			if(polygonArea == 0 || contourArea == 0) {
+				continue;
+			}
+			double percentFilled = polygonArea/contourArea*100;
+			if(percentFilled < 70) {
+				continue;
+			}
+
+			if(contourArea > largestContourArea) {
+				secondLargestContourArea = largestContourArea;
+				secondLargestContour = largestContour;
+				largestContourArea = contourArea;
+				largestContour = currentContour;
+			} else if(contourArea > secondLargestContourArea) {
+				secondLargestContourArea = contourArea;
+				secondLargestContour = currentContour;
+			}
+		}
+		cv::vector<cv::vector<cv::Point> > goodcontours = cv::vector<cv::vector<cv::Point> >();
+		goodcontours.push_back(secondLargestContour);
+		goodcontours.push_back(largestContour);
+		std::cout << "Number of contours: " << goodcontours.size() << "\n";	
+		cv::Scalar color(0, 225, 0);
+		cv::drawContours(newimg, goodcontours, -1, color, CV_FILLED);
+	}
+
+	cv::imshow("Hello!", newimg);
 	cv::waitKey();
 }
