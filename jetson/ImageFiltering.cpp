91e126cc3ada79c98b427f2b8ca1982053a0cd09
Timothy Eng <timothyeng36@gmail.com>
2017-11-25 22:29:32 +0000

    Log how much time each operation takes.

--- jetson/ImageFiltering.cpp
+++ jetson/ImageFiltering.cpp
@@ -4,24 +4,36 @@
 class ImageFiltering {
 	public:
 	cv::Mat& filterImage(cv::Mat &img) {
+		clock_t t;
+		t = clock();
 		cv::gpu::GpuMat *src = new cv::gpu::GpuMat();
 		cv::gpu::GpuMat *dst = new cv::gpu::GpuMat();
+		t = clock() - t;
 
-		src->upload(img);
+		std::cout << "ImageFiltering (gpuinit) - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
+	
+		t = clock();
+		src->upload(img); //THIS TAKES A WHILE THE FIRST TIME
+		t = clock() - t;
+	
+		std::cout << "ImageFiltering (imgupload) - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
 
 		cv::gpu::cvtColor(*src, *dst, CV_BGR2HSV);
 
 		dst->download(img);
-
+			
 		int lowerh = 50;
 		int lowers = 200;
 		int lowerv = 30;
 		int higherh = 66;
 		int highers = 255;
 		int higherv = 255;
-
+		t = clock();
 		cv::inRange(img, cv::Scalar(lowerh, lowers, lowerv), cv::Scalar(higherh, highers, higherv), img);
-		
+		t = clock() - t;
+		std::cout << "ImageFiltering (inRange) - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
+
+
 		delete src;
 		delete dst;
 	
--- jetson/main.cpp
+++ jetson/main.cpp
@@ -4,6 +4,7 @@
 #include "ImageFiltering.cpp"
 #include "ImageProc.cpp"
 #include "TargetProc.cpp"
+#include <time.h>
 
 int round2(double);
 
@@ -11,23 +12,34 @@ int main() {
 	ImageFiltering imageFilter = ImageFiltering();
 	ImageProc imageProc = ImageProc();
 	TargetProc targetProc = TargetProc();
+	clock_t t;
 	while(1) {
 		cv::Mat img;
 		cv::Mat newimg(480, 640, CV_8UC3, cv::Scalar(0, 0, 0));
 
 		img = cv::imread("/home/ubuntu/src/jetson/faketargets.png", cv::IMREAD_COLOR);
-		
+		t = clock();	
 		img = imageFilter.filterImage(img);
+		t = clock() - t;
+		std::cout << "ImageFiltering - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
 		cv::vector<cv::vector<cv::Point> > contours;
 		cv::vector<cv::vector<cv::Point> > goodcontours;
 		cv::vector<cv::Vec4i> hierarchy;
 		int error = 0;
+		t = clock();
 		goodcontours = imageProc.procImage(img, contours, goodcontours, hierarchy, error);
+		t = clock() - t;
+		std::cout << "ImageProc - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
+
 		if(error == 1) {
 			return 1;
 		}
-		targetProc.procTarget(newimg, goodcontours);
-		cv::imshow("Hello!", newimg);
+		t = clock();
+		targetProc.procTarget(newimg, goodcontours, networking);
+		t = clock() - t;
+		std::cout << "TargetProc - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
+
+		//cv::imshow("Hello!", newimg);
 		char c = cv::waitKey(1);
 		if(c == 'q') {
 			break;
