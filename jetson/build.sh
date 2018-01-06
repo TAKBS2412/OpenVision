a77d0891449bbe0371a79a63795d001e035ca2e5
Timothy Eng <timothyeng36@gmail.com>
2018-01-05 21:29:27 -0800

    Remove the old test program and run the new one afterwards.

--- jetson/build.sh
+++ jetson/build.sh
@@ -1,1 +1,3 @@
-g++ -I/usr/local/include/opencv -I/usr/local/include/opencv2 -L/usr/local/lib -g -o test.out test.cpp -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_stitching -lopencv_gpu
+rm test.out
+g++ -I/usr/local/include/opencv -I/usr/local/include/opencv2 -L/usr/local/lib -g -o test.out test.cpp -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_videoio -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_stitching -lopencv_gpu
+./test.out
