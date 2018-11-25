b70dcf37de85db19ad4b6b3f543ecd43220a9f50
Timothy Eng <timothyeng36@gmail.com>
2018-11-24 16:25:20 -0800

    Updated ImageFiltering for raspberry pi.

--- c++/ImageFiltering.cpp
+++ c++/ImageFiltering.cpp
@@ -1,41 +1,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/gpu/gpu.hpp>
 
 class ImageFiltering {
-	private:
-	cv::gpu::GpuMat inRange(cv::gpu::GpuMat *img, cv::Scalar lower, cv::Scalar higher) {
-		cv::gpu::GpuMat shsv[3];
-		cv::gpu::GpuMat thresch[3];
-		cv::gpu::GpuMat threscl[3];
-		cv::gpu::GpuMat thresc[3];
-		cv::gpu::GpuMat temp;
-		cv::gpu::GpuMat thres;
-
-		//Split HSV 3 channels
-		cv::gpu::split(*img, shsv);
-
-		//Threshold HSV channels
-		cv::gpu::threshold(shsv[0], thresch[0], lower[0], 255, cv::THRESH_BINARY);
-		cv::gpu::threshold(shsv[1], thresch[1], lower[1], 255, cv::THRESH_BINARY);
-		cv::gpu::threshold(shsv[2], thresch[2], lower[2], 255, cv::THRESH_BINARY);
-
-		cv::gpu::threshold(shsv[0], threscl[0], higher[0], 255, cv::THRESH_BINARY_INV);
-		cv::gpu::threshold(shsv[1], threscl[1], higher[1], 255, cv::THRESH_BINARY_INV);
-		cv::gpu::threshold(shsv[2], threscl[2], higher[2], 255, cv::THRESH_BINARY_INV);
-
-		cv::gpu::bitwise_and(thresch[0], threscl[0], thresc[0]);
-		cv::gpu::bitwise_and(thresch[1], threscl[1], thresc[1]);
-		cv::gpu::bitwise_and(thresch[2], threscl[2], thresc[2]);
-
-		//Bitwise AND the channels
-		cv::gpu::bitwise_and(thresc[0], thresc[1],temp);
-		cv::gpu::bitwise_and(temp, thresc[2], thres);
-		
-		*img = thres;
-		return thres;
-	}
 	public:
-	cv::Mat& filterImage(cv::Mat &img) {
+	void filterImage(cv::Mat &img, cv::Mat &newimg) {
 		clock_t t;
 		int lowerh = 50;
 		int lowers = 200;
@@ -43,25 +10,12 @@ class ImageFiltering {
 		int higherh = 66;
 		int highers = 255;
 		int higherv = 255;
+	
+		cv::cvtColor(img, newimg, CV_BGR2HSV);
 		
-		cv::gpu::GpuMat *src = new cv::gpu::GpuMat();
-		cv::gpu::GpuMat *dst = new cv::gpu::GpuMat();
-
-		src->upload(img);
-		
-		cv::gpu::cvtColor(*src, *dst, CV_BGR2HSV);
-		dst->download(img);
 		t = clock();
-		inRange(dst, cv::Scalar(lowerh, lowers, lowerv), cv::Scalar(higherh, highers, higherv));
-		//cv::inRange(img, cv::Scalar(lowerh, lowers, lowerv), cv::Scalar(higherh, highers, higherv), img);
+		cv::inRange(newimg, cv::Scalar(lowerh, lowers, lowerv), cv::Scalar(higherh, highers, higherv), newimg);
 		t = clock() - t;
 		std::cout << "ImageFiltering (inRange) - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
-
-		dst->download(img);
-		
-		delete src;
-		delete dst;
-	
-		return img;
 	}
 };
--- c++/main.cpp
+++ c++/main.cpp
@@ -3,20 +3,18 @@
 #include <iostream>
 #include "Networking.cpp"
 #include "ImageFiltering.cpp"
-#include "ImageProc.cpp"
-#include "TargetProc.cpp"
 #include <time.h>
 
 int round2(double);
 
 int main() {
 	ImageFiltering imageFilter = ImageFiltering();
-	ImageProc imageProc = ImageProc();
-	TargetProc targetProc = TargetProc();
+	//ImageProc imageProc = ImageProc();
+	//TargetProc targetProc = TargetProc();
 	clock_t t;
 	char hostname[] = "192.168.0.10"; //TODO Change this to 10.24.12.2
 	int port = 2412;
-	Networking networking = Networking(hostname, port);
+	//Networking networking = Networking(hostname, port);
 	const std::string cameraAddress = "http://192.168.0.11/mjpg/video.mjpg";
 	cv::VideoCapture vcap(cameraAddress);
 	while(1) {
@@ -25,11 +23,12 @@ int main() {
 
 		t = clock();
 		//img = cv::imread("/home/ubuntu/src/jetson/faketargets.png", cv::IMREAD_COLOR);
-		vcap >> img;	
-		img = imageFilter.filterImage(img);
+		//vcap >> img;	
+		img = cv::imread("../waamv/orig1.jpg");
+		imageFilter.filterImage(img, newimg);
 		t = clock() - t;
 		std::cout << "ImageFiltering - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
-		
+		/*
 		cv::vector<cv::vector<cv::Point> > contours;
 		cv::vector<cv::vector<cv::Point> > goodcontours;
 		cv::vector<cv::Vec4i> hierarchy;
@@ -47,12 +46,11 @@ int main() {
 		targetProc.procTarget(newimg, goodcontours, networking);
 		t = clock() - t;
 		std::cout << "TargetProc - Time elapsed (s): " << ((float)t)/CLOCKS_PER_SEC << "\n";
-
-		cv::imshow("Hello!", newimg);
-		goodcontours = imageProc.procImage(img, contours, goodcontours, hierarchy, &error);
+		*/
+		/*goodcontours = imageProc.procImage(img, contours, goodcontours, hierarchy, &error);
 		if(error == 0) {
 			targetProc.procTarget(newimg, goodcontours);
-		}
+		}*/
 		cv::imshow("Original Frame: ", img);
 		cv::imshow("Processed Frame: ", newimg);
 		char c = cv::waitKey(1);
@@ -60,5 +58,5 @@ int main() {
 			break;
 		}
 	}
-	networking.closeSocket();
+	//networking.closeSocket();
 }
