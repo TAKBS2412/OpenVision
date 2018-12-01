bca85ef291b49ac20fde8f545694572bc640840c
Timothy Eng <timothyeng36@gmail.com>
2018-11-30 19:12:41 -0800

    Determine type by checking first character in string.

--- c++/Constants.cpp
+++ c++/Constants.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <string>
 
+std::string getType(std::string input) {
+    if('0' <= input[0] && input[0] <= '9') {
+        for(std::string ::size_type i = 0; i < input.size(); i++) {
+            if(input[i] == '.') {
+                return "Double";
+            }
+        }
+        return "Integer";
+    }
+    switch(input[0]) {
+        case '\"':
+            return "String";
+        case '[':
+            return "Array";
+        case 'T':
+        case 'F':
+            return "Boolean";
+        default:
+            return "Invalid type";
+    }
+}
+
 int main() {
 	std::string input = "";
 	do {
 		std::cout << "Enter input: \n";
 		getline (std::cin, input);
-		std::cout << input << "\n";
+		std::cout << getType(input) << "\n";
 	} while(input!= "");
 }
