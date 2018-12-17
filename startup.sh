325a8e00e0a31d918703cc9b8422515b1c8564ae
Timothy Eng <timothyeng36@gmail.com>
2018-12-16 16:15:17 -0800

    Specify settings file in startup.sh

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ workon cv
 ping 10.24.12.1 -c 10 > /home/pi/src/logs/ping.log
 
 echo "---`date`---" >> /home/pi/src/logs/output.log
-python ~/src/competition/main.py >> /home/pi/src/logs/output.log 2>&1
+python ~/src/competition/main.py ~/src/competition/fast_settings >> /home/pi/src/logs/output.log 2>&1
