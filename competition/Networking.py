772672de798eaf3cba6da7b3fb4b09f08d518b10
Timothy Eng <timothyeng36@gmail.com>
2019-02-10 22:44:27 -0800

    Print an error message if the wrong networking protocol is specified.

--- competition/Networking.py
+++ competition/Networking.py
@@ -1,6 +1,6 @@
 import socket
 from networktables import NetworkTables
-
+import printer
 '''
 This class sends messages to the roboRIO via UDP.
 '''
@@ -14,6 +14,8 @@ class Networking:
 			self.sd = NetworkTables.getTable(constants.getValue("tablename"))
 		elif constants.getValue("protocol") == "UDP":
 			self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		else:
+			printer.printIfNeeded("Invalid protocol specified! Valid protocols are NT and UDP.", constants)
 
 	# Sends data to the roboRIO, using UDP or NetworkTables, depending on the value of protocol in constants.
 	def sendData(self, constants, angle, distance, doextake, targetsFound):
@@ -21,6 +23,8 @@ class Networking:
 			self.sendDataNT(self.sd, angle, distance, doextake, targetsFound)
 		elif constants.getValue("protocol") == "UDP":
 			self.sendDataUDP(angle, distance, doextake, targetsFound)
+		else:
+			printer.printIfNeeded("Invalid protocol specified! Valid protocols are NT and UDP.", constants)
 
 	# Sends data to the roboRIO via UDP (this is currently blocking)
 	def sendDataUDP(self, angle, distance, doextake, targetsFound):
