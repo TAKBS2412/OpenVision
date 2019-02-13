b6585d950da9e607dac36e3ec9df4788b8e55dca
Timothy Eng <timothyeng36@gmail.com>
2019-02-12 19:39:57 -0800

    Created settings file for competition use.

--- startup.sh
+++ startup.sh
@@ -6,5 +6,5 @@ workon cv
 ping 10.24.12.1 -c 10 > /home/pi/src/logs/ping.log
 
 echo "---`date`---" >> /home/pi/src/logs/output.log
-#python ~/src/competition/main.py ~/src/competition/fast_settings >> /home/pi/src/logs/output.log 2>&1
+#python ~/src/competition/main.py ~/src/competition/competition_settings >> /home/pi/src/logs/output.log 2>&1
 python ~/src/competition/main.py ~/src/competition/quit_settings >> /home/pi/src/logs/output.log 2>&1
